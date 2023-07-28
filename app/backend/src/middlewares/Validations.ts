fcdb0529a27d1b513b18bd6e70c45839100ca936
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-07-28 09:06:41 -0300

    REQ21 - Finalizado

--- app/backend/src/middlewares/Validations.ts
+++ app/backend/src/middlewares/Validations.ts
@@ -13,7 +13,6 @@ class Validations {
     const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     if (!email) return res.status(400).json({ message: 'All fields must be filled' });
     if (!password) return res.status(400).json({ message: 'All fields must be filled' });
-    console.log(!regex.test('invalid.user@user.com"'));
     if (!regex.test(email)) return res.status(401).json({ message: 'Invalid email or password' });
     if (password.length < 6) return res.status(401).json({ message: 'Invalid email or password' });
     next();
